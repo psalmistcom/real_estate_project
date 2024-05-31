b4560218e47c3eb54519d75eddf8e0ea7b4d8710
psalmistcom <dadewale9@gmail.com>
2024-05-31 17:18:20 +0100

    frontend setup

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,11 +1,18 @@
-import GuestLayout from "@/Layouts/GuestLayout";
+import Button from "@/Components/Button";
+import ButtonGradient from "@/Components/svg/ButtonGradient";
 import { Head } from "@inertiajs/react";
 
 export default function Home() {
     return (
         <>
-            <Head title="Home Page" />
-            <div>Home</div>
+            <Head title="Home page" />
+            <h1 className="text-3xl font-bold underline"> Hello World!</h1>
+            <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+                <Button className="mt-10" href="#login">
+                    Something
+                </Button>
+            </div>
+            <ButtonGradient />
         </>
     );
 }
